34a820009ccc877f126eefa6d1296f2fb36499f6
mark <markhinojosa1212@gmail.com>
2019-12-23 17:27:24 -0600

    added last minute shopping banner

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -16,14 +16,54 @@ const Featured = () => {
         <div style={{ width: "100%", display: "flex" }}>
           <img
             src={FeaturedDeal}
-            style={{ objectFit: "contain", height: "100%", width: "100%" }}
+            style={{
+              objectFit: "contain",
+              height: "100%",
+              width: "100%",
+              paddingRight: "5px",
+              alignSelf: "center"
+            }}
           />{" "}
         </div>
         <div style={{ width: "100%", display: "flex" }}>
           {" "}
           <img
             src={WeekleyDealsFeatured}
-            style={{ objectFit: "contain", height: "100%", width: "100%" }}
+            style={{
+              objectFit: "contain",
+              height: "100%",
+              width: "100%",
+              paddingLeft: "5px"
+            }}
+          />
+        </div>
+      </div>
+      <div style={styles.bannerRow}>
+        <img style={{ width: "100%" }} src={LastMinuteShopping} />
+      </div>
+      <div style={styles.dealsRow}>
+        <div style={{ width: "100%", display: "flex" }}>
+          <img
+            style={{
+              objectFit: "contain",
+              height: "100%",
+              width: "100%",
+
+              alignSelf: "center"
+            }}
+            src={HelmetFeatured}
+          />
+        </div>
+        <div style={{ width: "100%", display: "flex" }}>
+          <img
+            style={{
+              objectFit: "contain",
+              height: "100%",
+              width: "100%",
+
+              alignSelf: "center"
+            }}
+            src={StockingStuffers}
           />
         </div>
       </div>
@@ -32,23 +72,36 @@ const Featured = () => {
 };
 const styles = {
   featuredContainer: {
+    borderColor: "red",
+    borderWidth: "3px",
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "column",
     flex: 1,
     width: "100%",
     height: "100%",
     backgroundColor: "green",
-    alignContent: "center"
+    alignContent: "center",
+    flexDirection: "column"
   },
   dealsRow: {
     height: "30vh",
     width: "70%",
     display: "flex",
     flexDirection: "row",
-    justifyContent: "space-around",
+    // justifyContent: "space-around",
     alignContent: "center",
     marginTop: "5%",
     alignSelf: "center"
+  },
+  bannerRow: {
+    height: "30%",
+    width: "70%",
+    display: "flex",
+    flexDirection: "row",
+    alignContent: "center",
+    marginTop: "5%",
+    alignSelf: "center",
+    alignItems: "center"
   }
 };
 
