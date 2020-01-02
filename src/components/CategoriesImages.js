50dc655f94e1b2e3a0f4a7078eb56ebe125701b4
mark <markhinojosa1212@gmail.com>
2020-01-02 10:41:32 -0600

    added last 2

--- src/components/CategoriesImages.js
+++ src/components/CategoriesImages.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
 import {
   CategoryAccessories,
   CategoryAirbagGear,
@@ -39,31 +35,8 @@ const CategoriesImages = () => {
   const classes = useStyles();
 
   return (
-    <div
-      style={{
-        width: "100%",
-        // height: "100%",
-        display: "flex",
-        flex: 1,
-        justifyContent: "center",
-        backgroundColor: "#FAFAFA"
-      }}
-    >
-      <div
-        style={{
-          width: "70%",
-          borderColor: "blue",
-          borderStyle: "solid",
-          display: "grid",
-          //   height: "100px",
-          gridTemplateColumns: " 1fr 1fr",
-          gridRowsColumns: " 1fr 1fr",
-          gridColumnGap: "1%",
-          gridRowGap: "1%",
-          marginTop: "5%",
-          paddingBottom: "3%"
-        }}
-      >
+    <div style={styles.categoriesOuterContainer}>
+      <div style={styles.categoriesGridContainer}>
         <Card className={classes.card} style={styles.categoryStyling}>
           <div
             style={{
@@ -172,6 +145,27 @@ const CategoriesImages = () => {
 };
 
 const styles = {
+  categoriesOuterContainer: {
+    width: "100%",
+    // height: "100%",
+    display: "flex",
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#FAFAFA"
+  },
+  categoriesGridContainer: {
+    width: "70%",
+    borderColor: "blue",
+    borderStyle: "solid",
+    display: "grid",
+    //   height: "100px",
+    gridTemplateColumns: " 1fr 1fr",
+    gridRowsColumns: " 1fr 1fr",
+    gridColumnGap: "1%",
+    gridRowGap: "1%",
+    marginTop: "5%",
+    paddingBottom: "3%"
+  },
   categoryStyling: { height: "30vh", padding: "2%" },
   divWithBackgroundImage: {
     height: "100%",
