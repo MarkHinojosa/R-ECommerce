5129f58ab94e3867b650590d2023c4aa66ed5ef8
mark <markhinojosa1212@gmail.com>
2020-01-06 23:58:44 -0600

    setting up containers for brands carousel

--- src/components/BrandsCarousel.js
+++ src/components/BrandsCarousel.js
@@ -1,7 +1,45 @@
 import React from "react";
 
 const BrandsCarousel = () => {
-  return <div>Brands Carousel</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        backgroundColor: "red",
+        width: "100%",
+        alignSelf: "center",
+        justifyContent: "center"
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
+          backgroundColor: "lime",
+          width: "70%",
+          alignSelf: "center"
+        }}
+      >
+        <div
+          style={{ height: "100px", width: "100px", backgroundColor: "blue" }}
+        ></div>
+        <div
+          style={{ height: "100px", width: "100px", backgroundColor: "yellow" }}
+        ></div>
+        <div
+          style={{ height: "100px", width: "100px", backgroundColor: "green" }}
+        ></div>
+        <div
+          style={{ height: "100px", width: "100px", backgroundColor: "orange" }}
+        ></div>
+        <div
+          style={{ height: "100px", width: "100px", backgroundColor: "pink" }}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default BrandsCarousel;
